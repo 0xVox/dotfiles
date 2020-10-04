eb83523d1b3653186d158ca8cb865795e8c322f1
0xVox <tom.brew3@gmail.com>
2020-10-04 18:44:33 +0100

    Add tmux and powerline

--- setup.sh
+++ setup.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
 # bash 'strict mode'
-set -euo pipefail
+#set -euo pipefail
 
 # stow convenience function
 _stow() {
   stow -t ~ $1
 }
 
-# Install ZSH
-sudo apt install zsh direnv
+export PATH=$PATH:$HOME/bin
 
-CONFIGS=(zsh )
+# Install ZSH and tools
+sudo apt install zsh direnv stow tmux git
+
+# Tmux plugins
+mkdir -p ~/.tmux/plugins/tpm
+git clone git@github.com:tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone git@github.com:seebi/tmux-colors-solarized.git ~/.tmux/plugins/tmux-colors-solarized
+git clone git@github.com:tmux-plugins/tmux-resurrect.git ~/.tmux/plugins/tmux-resurrect 
+git clone git@github.com:tmux-plugins/tmux-continuum.git ~/.tmux/plugins/tmux-continuum 
+git clone git@github.com:erikw/tmux-powerline.git ~/.tmux/bin/tmux-powerline
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# Fix fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p ~/.local/share/fonts
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+fc-cache -vf ~/.local/share/fonts/
+
 OMZ="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 
 [ ! -d $HOME/.oh-my-zsh ] && RUNZSH=no sh -c "$(curl -fsSL $OMZ)" && rm $HOME/.zshrc
